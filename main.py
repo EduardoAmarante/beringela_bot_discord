e52862122aa99380a11036fcb1ed29b550b1a8e8
Eduardo Amarante <eamarante80@gmail.com>
2022-10-16 12:18:39 -0300

    intents update

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
+from operator import truediv
 import discord
-from time import sleep
 from discord.ext import commands
-from discord.ext import tasks
-import asyncio
-import requests
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import os
 
 
-# from dotenv import load_dotenv
-# load_dotenv()
-# CLIENT_ID_TWITCH = os.getenv("CLIENT_ID_TWITCH")
-# DISCORD_GUILD = os.getenv("DISCORD_GUILD")
-# TOKEN_DISCORD = os.getenv("TOKEN_DISCORD")
-# TWITCH_SECRET = os.getenv("TWITCH_SECRET")
+from dotenv import load_dotenv
+load_dotenv()
+CLIENT_ID_TWITCH = os.getenv("CLIENT_ID_TWITCH")
+DISCORD_GUILD = os.getenv("DISCORD_GUILD")
+TOKEN_DISCORD = os.getenv("TOKEN_DISCORD")
+TWITCH_SECRET = os.getenv("TWITCH_SECRET")
 
-CLIENT_ID_TWITCH = os.environ.get("CLIENT_ID_TWITCH")
-DISCORD_GUILD = os.environ.get("DISCORD_GUILD")
-TOKEN_DISCORD = os.environ.get("TOKEN_DISCORD")
-TWITCH_SECRET = os.environ.get("TWITCH_SECRET")
+# CLIENT_ID_TWITCH = os.environ.get("CLIENT_ID_TWITCH")
+# DISCORD_GUILD = os.environ.get("DISCORD_GUILD")
+# TOKEN_DISCORD = os.environ.get("TOKEN_DISCORD")
+# TWITCH_SECRET = os.environ.get("TWITCH_SECRET")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.dm_messages = True
+intents.guild_messages = True
+intents.reactions = True
+
 bot = commands.Bot(command_prefix='!',intents=intents)
+
 @bot.event
 async def on_ready():
     print('on ready')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
 @bot.command()
 async def clear(ctx, amount=0):
@@ -45,9 +51,8 @@ async def clear(ctx, amount=0):
         await ctx.send("You need to be an admin to use this command")
 
 @bot.command()
-async def pontuar(ctx):
-    name = ctx.author.name
-    print("{} marcou 10 pontos!".format(name))
+async def ping(ctx):
+    await ctx.send('pong')
 
 @bot.event
 async def on_message(message: discord.Message):
